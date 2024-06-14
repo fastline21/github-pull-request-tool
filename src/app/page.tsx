0b9ae4c974011b3de5f422d30a611e52b1b96aff
Joshwa Facistol <joshwafacistol.work@gmail.com>
2024-06-14 10:21:37 +0800

    fixed show/hide icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,17 @@ import { format } from 'date-fns';
 import { GithubFormData, GithubResponse } from '@/interfaces/github.interface';
 
 import { getMergePullRequest } from '@/controllers/github.controller';
-import { Key } from 'react';
+import { Key, useState } from 'react';
 
 const siteTitle = process.env.NEXT_PUBLIC_SITE_TITLE;
 
 const Home = () => {
+	const [isSensitiveContent, setIsSensitiveContent] = useState(true);
+
+	const handleSensitiveContent = () => {
+		setIsSensitiveContent(!isSensitiveContent);
+	};
+
 	const githubDefaultFormData = {
 		personal_access_token: '',
 		organization: '',
@@ -43,18 +49,96 @@ const Home = () => {
 				<form onSubmit={GithubForm.handleSubmit(onSubmit)}>
 					<div className='grid grid-cols-2 gap-4'>
 						<div className='mt-5'>
-							<input
-								type='text'
-								{...GithubForm.register(
-									'personal_access_token',
-									{
-										required:
-											'Personal Access Token is required',
+							<div className='relative'>
+								<input
+									type={
+										isSensitiveContent ? 'password' : 'text'
 									}
-								)}
-								className='border px-5 py-4 text-md rounded-md border-slate-400 block w-full'
-								placeholder='Personal Access Token'
-							/>
+									{...GithubForm.register(
+										'personal_access_token',
+										{
+											required:
+												'Personal Access Token is required',
+										}
+									)}
+									className='border px-5 py-4 text-md rounded-md border-slate-400 block w-full disabled:pointer-events-none'
+									placeholder='Personal Access Token'
+									id='hs-toggle-password'
+								/>
+								<button
+									type='button'
+									className={`absolute top-0 end-0 p-5 rounded-e-md${
+										isSensitiveContent ? ' active' : ''
+									}`}
+									onClick={handleSensitiveContent}
+								>
+									<svg
+										className='flex-shrink-0 size-3.5 text-gray-400 dark:text-neutral-600'
+										width='24'
+										height='24'
+										viewBox='0 0 24 24'
+										fill='none'
+										stroke='currentColor'
+										strokeWidth='2'
+										strokeLinecap='round'
+										strokeLinejoin='round'
+									>
+										<path
+											className={
+												!isSensitiveContent
+													? 'hidden'
+													: ''
+											}
+											d='M9.88 9.88a3 3 0 1 0 4.24 4.24'
+										></path>
+										<path
+											className={
+												!isSensitiveContent
+													? 'hidden'
+													: ''
+											}
+											d='M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68'
+										></path>
+										<path
+											className={
+												!isSensitiveContent
+													? 'hidden'
+													: ''
+											}
+											d='M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61'
+										></path>
+										<line
+											className={
+												!isSensitiveContent
+													? 'hidden'
+													: ''
+											}
+											x1='2'
+											x2='22'
+											y1='2'
+											y2='22'
+										></line>
+										<path
+											className={
+												isSensitiveContent
+													? 'block'
+													: ''
+											}
+											d='M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z'
+										></path>
+										<circle
+											className={
+												isSensitiveContent
+													? 'block'
+													: ''
+											}
+											cx='12'
+											cy='12'
+											r='3'
+										></circle>
+									</svg>
+								</button>
+							</div>
 						</div>
 						<div className='mt-5'>
 							<input
